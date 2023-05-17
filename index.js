fb45fbb508353bf176293487ed30c6a1d41a6b80
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2023-05-17 13:33:08 +0530

    paymentCommit

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ import userRouter from './src/routes/user.route.js';
 import logRouter from './src/routes/log.route.js';
 import cors from 'cors';
 import accRoute from './src/routes/account.route.js';
+import Stripe from "stripe";
+import dotenv from "dotenv";
 //import userRouter from './src/controllers/user.control';
 //import userRouter from './src/controllers/user.control';
 //const userRouter = require('./src/controllers/user.control');
 // require("dotenv").config();    
+
+const env = dotenv.config({ path: "./.env" });
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
 const app =  express();
 app.use(cors());
 app.use(bodyParser.json());
 const url = 'mongodb+srv://userBank:1234@bankapp.lrmrpna.mongodb.net/?retryWrites=true&w=majority';
+app.use(express.static(process.env.STATIC_DIR));
 const port = 5000;
 
 async function connectDB(url , connectionParams){
@@ -37,6 +47,62 @@ connectDB(url , {}).then(()=>{
 app.use(userRouter)
 app.use('/log' , logRouter)
 app.use('/acc' , accRoute)
+
+app.get("/config", (req, res) => {
+    res.send({
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  });
+
+
+  app.post("/create-payment-intent", async (req, res) => {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "INR",
+        amount: 50,
+        automatic_payment_methods: { enabled: true },
+      });
+  
+      // Send publishable key and PaymentIntent details to client
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (e) {
+      return res.status(400).send({
+        error: {
+          message: e.message,
+        },
+      });
+    }
+  });
+  
+// async function connectDb(){
+
+//     await mongoose.connect(url);
+
+// }
+
+// connectDb().then(async() => {
+//     console.log('Database Connected');
+//     app.listen(port , ()=>{
+//         console.log("Listening on port 3000");
+//     });
+    
+// }).catch((err) => {
+//     console.log('Connection Error' , err);
+// })
+
+
+
+
+
+
+// app.listen(port , ()=>{
+//         console.log("Listening on port 3000");
+// });
+ 
+          
+
 // async function connectDb(){
 
 //     await mongoose.connect(url);
